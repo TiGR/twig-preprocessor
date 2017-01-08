6b82e393dc805c7e6f135770f17967c98c9dc2b7
Igor Tarasov <tarasov.igor@gmail.com>
2017-01-08 23:30:14 +0400

    Initial commit (Twig 1 version)

---
+++ tests/PreprocessorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Twig_Tests_Loader_PreprocessorTest extends PHPUnit_Framework_TestCase
+{
+    public function testProcessing()
+    {
+        $realLoader = new Twig_Loader_Array(['test' => 'test']);
+        $loader = new Twig_Loader_Preprocessor($realLoader, 'strtoupper');
+        $this->assertEquals('TEST', $loader->getSource('test'));
+    }
+
+    public function testExists()
+    {
+        $realLoader = $this->getMockBuilder('Twig_Loader_Array')
+            ->setMethods(array('exists', 'getSource'))
+            ->disableOriginalConstructor()
+            ->getMock();
+        $realLoader->expects($this->once())->method('exists')->will($this->returnValue(false));
+        $realLoader->expects($this->never())->method('getSource');
+
+        /** @noinspection PhpParamsInspection */
+        $loader = new Twig_Loader_Preprocessor($realLoader, 'trim');
+        $this->assertFalse($loader->exists('foo'));
+
+        $realLoader = $this->createMock('Twig_LoaderInterface');
+        $realLoader->expects($this->once())->method('getSource')->will($this->returnValue('content'));
+
+        /** @noinspection PhpParamsInspection */
+        $loader = new Twig_Loader_Preprocessor($realLoader, 'trim');
+        $this->assertTrue($loader->exists('foo'));
+    }
+}
