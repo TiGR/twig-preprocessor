6b5a795f48ee544237891a39cc74e91500b65659
Igor Tarasov <tarasov.igor@gmail.com>
2017-01-09 13:28:31 +0400

    Remove bogus Twig copyright notice.

--- tests/PreprocessorTest.php
+++ tests/PreprocessorTest.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of Twig.
- *
- * (c) Fabien Potencier
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 class Twig_Tests_Loader_PreprocessorTest extends PHPUnit_Framework_TestCase
 {
     public function testProcessing()
