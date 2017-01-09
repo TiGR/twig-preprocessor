e44ea16b3ed67b8b1503383c2c63227f0106c7b8
Igor Tarasov <tarasov.igor@gmail.com>
2017-01-09 13:28:31 +0400

    Remove bogus Twig copyright notice.

--- lib/Twig_Loader_Preprocessor.php
+++ lib/Twig_Loader_Preprocessor.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of Twig.
- *
- * (c) 2011 Fabien Potencier
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /**
  * Twig Preprocessor loader that allows adding custom text filters for template strings.
  *
